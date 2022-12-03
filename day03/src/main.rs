9c22cdcf1d26dd373dfa2765a26aa11000874a72
Jacky <oechslein@gmx.de>
2022-12-03 10:53:00 +0100

    day03 part 1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -9,9 +9,7 @@
     clippy::must_use_candidate
 )]
 
-use rayon::prelude::*;
-
-use std::{cmp::Reverse, str::FromStr};
+use std::{cmp::Reverse, collections::HashSet, str::FromStr};
 
 use itertools::Itertools;
 use utils::{self, str_to};
@@ -21,164 +19,80 @@ use utils::{self, str_to};
 /// AOC
 fn main() {
     utils::with_measure("Part 1", || solve_part1("day03/input.txt"));
-    utils::with_measure("Part 2", || solve_part2("day03/input.txt"));
+    utils::with_measure("Part 2", || solve_part2("day03/test.txt"));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 pub fn solve_part1(file_name: &str) -> usize {
-    parse_input_part(&utils::file_to_string(file_name))
-        .map(Move::set_round_outcome)
-        .map(Move::player_score)
-        .sum()
+    parse_input_part_1(&utils::file_to_string(file_name)).map(sum_of_types_priority).sum()
 }
 
 pub fn solve_part2(file_name: &str) -> usize {
-    parse_input_part(&utils::file_to_string(file_name))
-        .map(Move::set_player_move)
-        .map(|x| Move::player_score(x))
-        .sum()
+    let input = utils::file_to_string(file_name);
+    for (rucksack1, rucksack2, rucksack3) in parse_input_part_2(&input) {
+        let rucksack1_set: HashSet<_> = HashSet::from_iter(rucksack1.iter());
+        let rucksack2_set: HashSet<_> = HashSet::from_iter(rucksack2.iter());
+        let rucksack3_set: HashSet<_> = HashSet::from_iter(rucksack3.iter());
+
+        let common_1_2 = rucksack1_set.intersection(&rucksack2_set).map(|x| *x).collect::<HashSet<_>>();
+        let x = common_1_2.intersection(&rucksack3_set);
+        x.map(|x| type_priority(**x)).sum::<usize>();
+        println!("{:?}", x);    
+    }
+    42
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-fn parse_input_part<'a>(input: &'a String) -> impl Iterator<Item = Move> + 'a {
+type Group = (RucksackPart2, RucksackPart2, RucksackPart2);
+type RucksackPart2 = Vec<char>;
+
+fn parse_input_part_2<'a>(
+    input: &'a String,
+) -> impl Iterator<Item = Group> + 'a {
     input
         .split('\n')
-        .filter_map(|line| Move::from_str(line).ok())
+        .map(|line| {
+                line.chars().collect_vec()
+        })
+        .tuples::<Group>()
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+type RucksackPart1 = (Compartment, Compartment);
+type Compartment = Vec<char>;
+
+fn parse_input_part_1<'a>(input: &'a String) -> impl Iterator<Item = RucksackPart1> + 'a {
+    input
+        .split('\n')
+        .map(|line| {
+            (
+                line[0..line.len() / 2].chars().collect_vec(),
+                line[line.len() / 2..line.len()].chars().collect_vec(),
+            )
+        })
         .into_iter()
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-struct Move {
-    opponent_move: MoveEnum,
-    player_move: MoveEnum,
-    player_outcome: RoundOutcome,
+fn sum_of_types_priority((compart1, compart2): RucksackPart1) -> usize {
+    let compart1_set: HashSet<_> = HashSet::from_iter(compart1.iter());
+    let compart2_set: HashSet<_> = HashSet::from_iter(compart2.iter());
+    let common_parts = compart1_set.intersection(&compart2_set);
+    //println!("{:?},{:?}=>{:?}", compart1, compart2, common_parts.clone());
+    let sum_of_types_priority = common_parts
+        .into_iter()
+        .map(|x| type_priority(**x))
+        .sum::<u32>();
+    sum_of_types_priority as usize
 }
 
-impl FromStr for Move {
-    type Err = String;
-
-    fn from_str(line: &str) -> Result<Self, Self::Err> {
-        if let Some((first, second)) = line.trim().split(' ').collect_tuple() {
-            println!("{:?} {:?}", first, second);
-            Ok(Move {
-                opponent_move: MoveEnum::from_str(first).unwrap(),
-                player_move: MoveEnum::from_str(second).unwrap(),
-                player_outcome: RoundOutcome::from_str(second).unwrap(),
-            })
-        } else {
-            Err(line.to_string())
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum MoveEnum {
-    ROCK,
-    SCISSORS,
-    PAPER,
-}
-use MoveEnum::*;
-
-impl FromStr for MoveEnum {
-    type Err = String;
-
-    fn from_str(x: &str) -> Result<Self, Self::Err> {
-        let move_char = x.chars().next().unwrap();
-        if move_char == 'A' || move_char == 'X' {
-            Ok(ROCK)
-        } else if move_char == 'B' || move_char == 'Y' {
-            Ok(PAPER)
-        } else if move_char == 'C' || move_char == 'Z' {
-            Ok(SCISSORS)
-        } else {
-            Err(x.to_string())
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-enum RoundOutcome {
-    DRAW,
-    WIN,
-    LOSS,
-}
-
-use RoundOutcome::*;
-
-impl FromStr for RoundOutcome {
-    type Err = String;
-
-    fn from_str(x: &str) -> Result<Self, Self::Err> {
-        let move_char = x.chars().next().unwrap();
-        if move_char == 'X' {
-            Ok(LOSS)
-        } else if move_char == 'Y' {
-            Ok(DRAW)
-        } else if move_char == 'Z' {
-            Ok(WIN)
-        } else {
-            Err(x.to_string())
-        }
-    }
-}
-
-impl Move {
-    fn last_player_wins_p(&self) -> bool {
-        match self.player_move {
-            ROCK => self.opponent_move == SCISSORS,
-            SCISSORS => self.opponent_move == PAPER,
-            PAPER => self.opponent_move == ROCK,
-        }
-    }
-
-    fn set_round_outcome(mut self) -> Self {
-        self.player_outcome = {
-            if self.opponent_move == self.player_move {
-                DRAW
-            } else if self.last_player_wins_p() {
-                WIN
-            } else {
-                LOSS
-            }
-        };
-        self
-    }
-
-    fn set_player_move(mut self) -> Self {
-        self.player_move = match (self.player_outcome, self.opponent_move) {
-            (DRAW, _) => self.opponent_move,
-            (WIN, ROCK) => PAPER,
-            (WIN, SCISSORS) => ROCK,
-            (WIN, PAPER) => SCISSORS,
-            (LOSS, ROCK) => SCISSORS,
-            (LOSS, SCISSORS) => PAPER,
-            (LOSS, PAPER) => ROCK,
-        };
-        self
-    }
-
-    fn player_score(self) -> usize {
-        self.player_move.move_score() + self.outcome_score()
-    }
-
-    fn outcome_score(&self) -> usize {
-        match self.player_outcome {
-            LOSS => 0,
-            DRAW => 3,
-            WIN => 6,
-        }
-    }
-}
-
-impl MoveEnum {
-    fn move_score(&self) -> usize {
-        match self {
-            ROCK => 1,
-            PAPER => 2,
-            SCISSORS => 3,
-        }
+fn type_priority(x: char) -> u32 {
+    if x.is_lowercase() {
+        x as u32 - 96
+    } else {
+        x as u32 - 38
     }
 }
 
@@ -192,12 +106,12 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(solve_part1("test.txt"), 15);
+        assert_eq!(solve_part1("test.txt"), 157);
     }
 
     #[test]
     fn verify1() {
-        assert_eq!(solve_part1("input.txt"), 11449);
+        assert_eq!(solve_part1("input.txt"), 7872);
     }
 
     #[test]
