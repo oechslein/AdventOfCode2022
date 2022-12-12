8d99924434220da114cc40e1b46724dcf50cabca
Christoph Oechslein <oechslein@gmx.de>
2022-12-12 13:19:26 +0100

    day 12
    Change-Id: I077ff3231cfa32ff03b843cf5cc46e5c0527bb14

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -9,26 +9,101 @@
     clippy::must_use_candidate
 )]
 
+use grid::grid_array::{GridArray, GridArrayBuilder};
+use grid::grid_types::{Coor, Neighborhood, Topology};
 use itertools::Itertools;
+
+use pathfinding::prelude::dijkstra;
 
 ////////////////////////////////////////////////////////////////////////////////////
 /// The main function prints out the results for part1 and part2
 /// AOC
 fn main() {
-    utils::with_measure("Part 1", || solve_part1("day12/test.txt"));
+    utils::with_measure("Part 1", || solve_part1("day12/input.txt"));
     utils::with_measure("Part 2", || solve_part2("day12/input.txt"));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 pub fn solve_part1(file_name: &str) -> usize {
-    let _input = utils::file_to_lines(file_name);
-    42
+    let grid = parse_grid(file_name);
+
+    get_lowest_steps(
+        &grid,
+        find_first_pos(&grid, 'S'),
+        find_first_pos(&grid, 'E'),
+    )
+    .unwrap()
 }
 
 pub fn solve_part2(file_name: &str) -> usize {
-    let _input = utils::file_to_lines(file_name);
-    42
+    let grid = parse_grid(file_name);
+    let goal_pos = find_first_pos(&grid, 'E');
+
+    grid.all_cells()
+        .filter(|(_, c)| **c == 'S' || **c == 'a')
+        .filter_map(|start_pos| get_lowest_steps(&grid, start_pos.0, goal_pos))
+        .min()
+        .unwrap()
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+fn find_first_pos(grid: &GridArray<char>, find_char: char) -> Coor {
+    grid.all_cells()
+        .filter(|(_, c)| **c == find_char)
+        .next()
+        .unwrap()
+        .0
+}
+
+fn get_successor<'a>(
+    grid: &'a GridArray<char>,
+    coor: Coor,
+) -> impl IntoIterator<Item = (Coor, usize)> + 'a {
+    let curr_cell = (coor, grid.get(coor.0, coor.1).unwrap());
+    grid.neighborhood_cells(coor.0, coor.1)
+        .filter_map(move |neighbor_cell| get_weigth(curr_cell, neighbor_cell))
+}
+
+fn get_weigth(curr_cell: (Coor, &char), neighbor_cell: (Coor, &char)) -> Option<(Coor, usize)> {
+    let curr_cell_number = get_value(*curr_cell.1);
+    let neighbor_cell_number = get_value(*neighbor_cell.1);
+    if (neighbor_cell_number <= curr_cell_number) || (neighbor_cell_number == curr_cell_number + 1)
+    {
+        Some((neighbor_cell.0, 1))
+    } else {
+        None
+    }
+}
+
+fn get_value(cell: char) -> u32 {
+    match cell {
+        'S' => 'a' as u32,
+        'E' => 'z' as u32,
+        _ => cell as u32,
+    }
+}
+
+fn parse_grid(file_name: &str) -> GridArray<char> {
+    let input = utils::file_to_string(file_name);
+    let grid = GridArray::from_newline_separated_string(
+        Topology::Bounded,
+        Neighborhood::Orthogonal,
+        input,
+    );
+    //grid.print();
+    grid
+}
+
+fn get_lowest_steps(grid: &GridArray<char>, start_pos: Coor, goal_pos: Coor) -> Option<usize> {
+    let result = dijkstra(
+        &start_pos,
+        |coor| get_successor(&grid, *coor),
+        |coor| *coor == goal_pos,
+    );
+    //println!("{:?}", result);
+    result.map(|result| result.1)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -41,22 +116,22 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(solve_part1("test.txt"), 42);
+        assert_eq!(solve_part1("test.txt"), 31);
     }
 
     #[test]
     fn verify1() {
-        assert_eq!(solve_part1("input.txt"), 42);
+        assert_eq!(solve_part1("input.txt"), 352);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve_part2("test.txt"), 42);
+        assert_eq!(solve_part2("test.txt"), 29);
     }
 
     #[test]
     fn verify2() {
-        assert_eq!(solve_part2("input.txt"), 42);
+        assert_eq!(solve_part2("input.txt"), 345);
     }
 
     #[bench]
