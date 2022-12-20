7d44d014132a3b0fe46011337d1e9ba6144e3229
Christoph Oechslein <oechslein@gmx.de>
2022-12-20 13:04:27 +0100

    day20
    Change-Id: Idcf05a235035c68e521bde852ecc98ba13b094ad

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,6 +1,6 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
-#![allow(unused_must_use)]
+//#![allow(unused_imports)]
+//#![allow(dead_code)]
+//#![allow(unused_must_use)]
 #![feature(test)]
 #![deny(clippy::all, clippy::pedantic)]
 #![allow(
@@ -9,26 +9,59 @@
     clippy::must_use_candidate
 )]
 
-use itertools::Itertools;
-
 ////////////////////////////////////////////////////////////////////////////////////
 /// The main function prints out the results for part1 and part2
 /// AOC
 fn main() {
-    utils::with_measure("Part 1", || solve_part1("day20/test.txt"));
-    utils::with_measure("Part 2", || solve_part2("day20/test.txt"));
+    utils::with_measure("Part 1", || solve_part1("day20/input.txt"));
+    utils::with_measure("Part 2", || solve_part2("day20/input.txt"));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-pub fn solve_part1(file_name: &str) -> usize {
-    let _input = utils::file_to_lines(file_name);
-    42
+pub fn solve_part1(file_name: &str) -> isize {
+    solve(parse(file_name, 1), 1)
 }
 
-pub fn solve_part2(file_name: &str) -> usize {
-    let _input = utils::file_to_lines(file_name);
-    42
+pub fn solve_part2(file_name: &str) -> isize {
+    const DECRYPTION_KEY: isize = 811589153;
+    solve(parse(file_name, DECRYPTION_KEY), 10)
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+fn solve(original_input: Vec<(isize, usize)>, times: i32) -> isize {
+    let mut input = original_input.clone();
+    for _ in 0..times {
+        for value in original_input.iter() {
+            let index = input.iter().position(|x| *x == *value).unwrap();
+            rotate(&mut input, index);
+        }
+    }
+    compute_result(input)
+}
+
+fn compute_result(input: Vec<(isize, usize)>) -> isize {
+    let zero_pos = input.iter().position(|(x, _counter)| *x == 0).unwrap();
+    [1000, 2000, 3000]
+        .into_iter()
+        .map(|pos| input[(zero_pos + pos) % input.len()].0)
+        .sum()
+}
+
+fn rotate(input: &mut Vec<(isize, usize)>, index: usize) {
+    let value = input.remove(index);
+    // rust modulo is not the same as python modulo (-1 % 5 = -1 in rust, -1 % 5 = 4 in python) => rem_euclid
+    let new_index = ((index as isize) + value.0).rem_euclid(input.len() as isize) as usize;
+    input.insert(new_index, value);
+}
+
+fn parse(file_name: &str, decryption_key: isize) -> Vec<(isize, usize)> {
+    utils::file_to_lines(file_name)
+        .map(move |line| utils::str_to::<isize>(line.as_str()) * decryption_key)
+        .enumerate()
+        .map(|(index, x)| (x, index))
+        .collect()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -41,22 +74,22 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(solve_part1("test.txt"), 42);
+        assert_eq!(solve_part1("test.txt"), 3);
     }
 
     #[test]
     fn verify1() {
-        assert_eq!(solve_part1("input.txt"), 42);
+        assert_eq!(solve_part1("input.txt"), 7004);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(solve_part2("test.txt"), 42);
+        assert_eq!(solve_part2("test.txt"), 1623178306);
     }
 
     #[test]
     fn verify2() {
-        assert_eq!(solve_part2("input.txt"), 42);
+        assert_eq!(solve_part2("input.txt"), 17200008919529);
     }
 
     #[bench]
