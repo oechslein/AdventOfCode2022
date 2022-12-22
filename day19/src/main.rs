cd98b023c6daf002f0f3a0e9f7c71fe209017646
Christoph Oechslein <oechslein@gmx.de>
2022-12-22 10:53:00 +0100

    Day 19 speed up
    Change-Id: I53fe72d97137bb61f020b08e6ab2ed503caf637d

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -9,10 +9,10 @@
     clippy::must_use_candidate
 )]
 
-use std::collections::VecDeque;
-use fxhash::FxHashSet;
+use fxhash::{FxHashMap, FxHashSet};
 use itertools::Itertools;
 use rayon::prelude::*;
+use std::collections::VecDeque;
 
 ////////////////////////////////////////////////////////////////////////////////////
 /// The main function prints out the results for part1 and part2
@@ -23,38 +23,66 @@ fn main() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-pub fn solve_part1(file_name: &str) -> usize {
+
+type UInt = u16;
+
+pub fn solve_part1(file_name: &str) -> UInt {
     let blueprints = parse_blueprints(file_name);
     //println!("{:?}", blueprints);
 
-    const LIMIT: usize = 24;
-
     blueprints
         .par_iter()
-        .map(|blueprint| blueprint.id * get_max_geodes(LIMIT, blueprint))
+        .map(|blueprint| blueprint.id * get_max_geodes(24, blueprint))
         .sum()
 }
 
-pub fn solve_part2(file_name: &str) -> usize {
-    let blueprints = parse_blueprints(file_name).into_iter().take(3).collect_vec();
+pub fn solve_part2(file_name: &str) -> UInt {
+    let blueprints = parse_blueprints(file_name)
+        .into_iter()
+        .take(3)
+        .collect_vec();
     //println!("{:?}", blueprints);
-
-    const LIMIT: usize = 32;
 
     blueprints
         .par_iter()
-        .map(|blueprint| get_max_geodes(LIMIT, blueprint))
+        .map(|blueprint| get_max_geodes(32, blueprint))
         .product()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-fn get_max_geodes(limit: usize, blueprint: &Blueprint) -> usize {
-    let mut max_geode_robot_per_time = vec![0; limit + 1];
+fn get_max_geodes(limit: UInt, blueprint: &Blueprint) -> UInt {
+    let mut max_geode_robot_per_time = vec![0; limit as usize + 1];
     let mut visited_nodes = FxHashSet::default();
     let mut open_nodes = VecDeque::new();
     open_nodes.push_front(Node::new());
     let mut finished_nodes = vec![];
+
+    let mut add_node_if_not_visited = |node: Node, open_nodes: &mut VecDeque<_>| {
+        if !visited_nodes.contains(&node) {
+            visited_nodes.insert(node.clone());
+            open_nodes.push_back(node);
+        } else {
+            //println!("Already visited: {:?}", new_node);
+        }
+    };
+
+    // Calculate the maximum amount for every type of bot so that the creation of a new bot of any type is never bottlenecked
+    let max_robots: FxHashMap<Robot, UInt> = Robot::iter()
+        .map(|robot_that_generate_element| {
+            (robot_that_generate_element, {
+                Robot::iter()
+                    .map(|robot| {
+                        blueprint.element_costs_for_robot(robot, robot_that_generate_element)
+                    })
+                    .filter(|cost| *cost != 0) // Genodes can't be created by any robot but needed of course
+                    .max()
+                    .unwrap_or(limit)
+            })
+        })
+        .collect();
+    //println!("max_robots: {:?}", max_robots);
+
     while let Some(mut node) = open_nodes.pop_front() {
         if node.spent_minutes >= limit {
             if false {
@@ -68,18 +96,25 @@ fn get_max_geodes(limit: usize, blueprint: &Blueprint) -> usize {
             continue;
         }
 
-        if max_geode_robot_per_time[node.spent_minutes] > node.robots[Element::Geode as usize] {
+        if max_geode_robot_per_time[node.spent_minutes as usize]
+            > node.robots[Robot::Geode as usize]
+        {
             //println!("Pruned: {:?} {}", node, max_geode_robot_per_time[node.spent_minutes]);
             continue;
         }
 
-        max_geode_robot_per_time[node.spent_minutes] = node.robots[Element::Geode as usize];
+        max_geode_robot_per_time[node.spent_minutes as usize] =
+            node.robots[Element::Geode as usize];
 
         node.spent_minutes += 1;
 
         // first check what robots are possible to buy
-        let possible_robots = Element::iter()
-            .filter(|robot| node.can_buy_robot(*robot, blueprint))
+        let possible_robots = Robot::iter()
+            .rev()
+            .filter(|robot| {
+                node.can_buy_robot(*robot, blueprint)
+                    && node.robots[*robot as usize] <= max_robots[robot]
+            })
             .collect_vec();
         // then collect
         node.robots_collect();
@@ -89,24 +124,13 @@ fn get_max_geodes(limit: usize, blueprint: &Blueprint) -> usize {
         for robot in possible_robots {
             let mut new_node = node.clone();
             new_node.buy_robot(robot, blueprint);
-            if !visited_nodes.contains(&new_node) {
-                visited_nodes.insert(new_node.clone());
-                open_nodes.push_back(new_node);
-            } else {
-                //println!("Already visited: {:?}", new_node);
-            }
+            add_node_if_not_visited(new_node, &mut open_nodes);
         }
 
         // option do nothing
-        if !visited_nodes.contains(&node) {
-            visited_nodes.insert(node.clone());
-            open_nodes.push_back(node);
-        } else {
-            //println!("Already visited: {:?}", node);
-        }
-
-        // todo prune nodes buy removing nodes with same time but less geode robots
+        add_node_if_not_visited(node, &mut open_nodes);
     }
+
     let max_node = finished_nodes
         .into_iter()
         .max_by_key(|node| node.elements[Element::Geode as usize])
@@ -121,7 +145,7 @@ fn get_max_geodes(limit: usize, blueprint: &Blueprint) -> usize {
 const ELEMENT_COUNT: usize = 4;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-#[repr(usize)]
+#[repr(u8)]
 enum Element {
     Ore = 0,
     Clay = 1,
@@ -129,8 +153,10 @@ enum Element {
     Geode = 3,
 }
 
+type Robot = Element;
+
 impl Element {
-    fn from_index(index: usize) -> Element {
+    fn from_index(index: UInt) -> Element {
         match index {
             0 => Element::Ore,
             1 => Element::Clay,
@@ -140,16 +166,16 @@ impl Element {
         }
     }
 
-    fn iter() -> impl Iterator<Item = Element> {
-        (0..ELEMENT_COUNT).map(Element::from_index)
+    fn iter() -> impl DoubleEndedIterator<Item = Element> {
+        (0..ELEMENT_COUNT as UInt).map(Element::from_index)
     }
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 struct Node {
-    elements: [usize; ELEMENT_COUNT],
-    robots: [usize; ELEMENT_COUNT],
-    spent_minutes: usize,
+    elements: [UInt; ELEMENT_COUNT],
+    robots: [UInt; ELEMENT_COUNT],
+    spent_minutes: UInt,
 }
 
 impl Node {
@@ -169,16 +195,16 @@ impl Node {
         }
     }
 
-    fn buy_robot(&mut self, robot: Element, blueprint: &Blueprint) {
+    fn buy_robot(&mut self, robot: Robot, blueprint: &Blueprint) {
         self.robots[robot as usize] += 1;
         for element in Element::iter() {
-            self.elements[element as usize] -= blueprint.elemen_costs_for_robot(robot, element);
+            self.elements[element as usize] -= blueprint.element_costs_for_robot(robot, element);
         }
     }
 
-    fn can_buy_robot(&mut self, robot: Element, blueprint: &Blueprint) -> bool {
+    fn can_buy_robot(&mut self, robot: Robot, blueprint: &Blueprint) -> bool {
         for element in Element::iter() {
-            if self.elements[element as usize] < blueprint.elemen_costs_for_robot(robot, element) {
+            if self.elements[element as usize] < blueprint.element_costs_for_robot(robot, element) {
                 return false;
             }
         }
@@ -190,22 +216,22 @@ impl Node {
 
 #[derive(Debug)]
 struct Blueprint {
-    id: usize,
-    ore_robot_cost: usize,               // = costs x ore
-    clay_robot_cost: usize,              // = costs x ore
-    obsidian_robot_cost: (usize, usize), // = costs x ore, y clay
-    geode_robot_cost: (usize, usize),    // = costs x ore, y obsidian
+    id: UInt,
+    ore_robot_cost: UInt,              // = costs x ore
+    clay_robot_cost: UInt,             // = costs x ore
+    obsidian_robot_cost: (UInt, UInt), // = costs x ore, y clay
+    geode_robot_cost: (UInt, UInt),    // = costs x ore, y obsidian
 }
 
 impl Blueprint {
-    fn elemen_costs_for_robot(&self, robot: Element, element: Element) -> usize {
+    fn element_costs_for_robot(&self, robot: Robot, element: Element) -> UInt {
         match (robot, element) {
-            (Element::Ore, Element::Ore) => self.ore_robot_cost,
-            (Element::Clay, Element::Ore) => self.clay_robot_cost,
-            (Element::Obsidian, Element::Ore) => self.obsidian_robot_cost.0,
-            (Element::Obsidian, Element::Clay) => self.obsidian_robot_cost.1,
-            (Element::Geode, Element::Ore) => self.geode_robot_cost.0,
-            (Element::Geode, Element::Obsidian) => self.geode_robot_cost.1,
+            (Robot::Ore, Element::Ore) => self.ore_robot_cost,
+            (Robot::Clay, Element::Ore) => self.clay_robot_cost,
+            (Robot::Obsidian, Element::Ore) => self.obsidian_robot_cost.0,
+            (Robot::Obsidian, Element::Clay) => self.obsidian_robot_cost.1,
+            (Robot::Geode, Element::Ore) => self.geode_robot_cost.0,
+            (Robot::Geode, Element::Obsidian) => self.geode_robot_cost.1,
             _ => 0,
         }
     }
@@ -214,7 +240,7 @@ impl Blueprint {
 fn parse_blueprints(file_name: &str) -> Vec<Blueprint> {
     let mut input = vec![];
     for line in utils::file_to_lines(file_name) {
-        let mut blueprint: Vec<(usize, usize)> = vec![];
+        let mut blueprint = vec![];
         let start_pos = line.find(": ").unwrap();
         let line = line
             .replace(".", "")
@@ -237,7 +263,7 @@ fn parse_blueprints(file_name: &str) -> Vec<Blueprint> {
         }
 
         let blueprint = Blueprint {
-            id: input.len() + 1,
+            id: input.len() as UInt + 1,
             ore_robot_cost: blueprint[0].0,
             clay_robot_cost: blueprint[1].0,
             obsidian_robot_cost: blueprint[2],
@@ -278,14 +304,11 @@ mod tests {
         assert_eq!(solve_part2("input.txt"), 14725);
     }
 
-    // Too slow
-    #[ignore]
     #[bench]
     fn benchmark_part1(b: &mut Bencher) {
         b.iter(|| solve_part1("input.txt"));
     }
 
-    #[ignore]
     #[bench]
     fn benchmark_part2(b: &mut Bencher) {
         b.iter(|| solve_part2("input.txt"));
